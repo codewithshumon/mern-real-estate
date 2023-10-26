438e2d94befe3cfdd2130b55a1cd5f5fb84e2572
Shumon Khan <backlink.shumonkhan@gmail.com>
2023-10-27 00:25:58 +0600

    created search api route

--- api/controllers/listingController.js
+++ api/controllers/listingController.js
@@ -65,3 +65,56 @@ export const getListing = async (req, res, next) => {
     next(error.message);
   }
 };
+
+export const searchListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+
+    const offer = parseInt(req.query.offer);
+    //here offer value could be fales/true/undefined.
+    //we are searching if offer value is "false" we need "offer= true& false" both listing
+    //we can use undefine with qoute "undefined" or without qoute undefined
+    if (offer === "undefined" || offer === "false")
+      offer = { $in: [true, false] };
+
+    const furnished = parseInt(req.query.furnished);
+    //here furnished value could be fales/true/undefined.
+    //same here if furnished value is "false" we need "furnished= true& false" both listing
+    if (furnished === undefined || furnished === "false")
+      furnished = { $in: [true, false] };
+
+    const parking = parseInt(req.query.parking);
+
+    //we can use undefine with qoute "undefined" or without qoute undefined
+    if (parking === "undefined" || parking === "false")
+      parking = { $in: [true, false] };
+
+    const type = parseInt(req.query.type);
+    //here type value could be sale/rent/undefined/all.
+    //same here if type value is "undefine or all" we need "type= sale & rent" listing
+    if (type === undefined || type === "false") type = { $in: [true, false] };
+
+    const searchTerm = req.query.searchTerm || "";
+
+    const sort = req.query.sort || "createdAt";
+
+    //if there is not 'searchTerm and order" then we will use "createdAt and descending"
+    const order = req.query.order || "desc";
+
+    const listing = await Listing.find({
+      title: { $regex: searchTerm, $options: "i" },
+    })
+      .sort({
+        //here sort means how it will sort listing data
+        [sort]: order,
+      })
+      .limit(limit) //here limit means how many data will show in search result per page
+      .skip(startIndex);
+    //here skip means here how many data will be not shown in search. Like here if startIndex is 0 then it start show from the begening but startIndex is 1, 2 or more then it will skipe "the amount (number) of data/listing that set to limit" like here default limit is 9. if startIndex is 1 then it will skip first 9 data/listing from shown
+
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error.message);
+  }
+};
